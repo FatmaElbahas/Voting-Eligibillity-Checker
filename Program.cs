815458188d6a213ef0a6fb9d8c044fa5e45648d3
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-07 23:14:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double age =Convert.ToDouble (Console.ReadLine());
+            if (age<18)
+            {
+                Console.WriteLine("You Are Not Eligible To Vote");
+
+            }
+            else
+            {
+                Console.WriteLine("You Are Eligible To Vote");
+
+            }
+        }
+    }
+}
